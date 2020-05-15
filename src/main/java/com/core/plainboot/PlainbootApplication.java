ad79323dddd1f46001841411429664ec3d121961
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-15 13:41:02 -0400

    commit

---
+++ src/main/java/com/core/plainboot/PlainbootApplication.java
@@ -0,0 +1,13 @@
+package com.core.plainboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlainbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlainbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/core/plainboot/PlainbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.plainboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlainbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
