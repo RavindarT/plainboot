0d02fb2264201d5a972fabee83ae4e3d850a6965
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-15 22:43:27 -0400

    commit

---
+++ src/main/java/com/core/plainboot/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.core.plainboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/test")
+public class FirstController {
+	
+	@GetMapping("/req")
+	public String getContent() {
+		return "Hello";
+	}
+}
